c1427624b7004f07a81ce7ea6db442cdd8f30523
Amit7304 <akrathore7304@gmail.com>
2026-07-06 00:26:26 +0530

    Contains Duplicate

--- Duplicate.java
+++ Duplicate.java
@@ -1,4 +1,9 @@
 public class Duplicate {
+
+
+
+
+    /***********************Brute-force aproach********************/
     public static boolean containsDuplicate(int[] nums) {
         for (int i=0; i<nums.length;i++){
            for(int j=i+1; j<nums.length;j++){
